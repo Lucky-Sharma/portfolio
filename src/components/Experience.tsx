cb06d3921dcbd3bd5893921e826cbf71a7f6acce
Lucky-Sharma <Luckysharmab1@gmail.com>
2025-11-03 11:15:21 +0530

    fix:experience section responsiveness

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -41,18 +41,20 @@ export const Experience = () => {
         <div className="font-bold text-4xl text-[#c6d0f0]">/experience</div>
         <div className="flex-1 h-[1px] bg-gray-600 ml-3"></div>
       </div>
-      <div className="h-screen flex justify-start gap-10 mt-10">
-        <div className=" relative flex flex-col w-1/3 md:w-90">
+      <div className="flex flex-col md:flex-row justify-start gap-10 mt-10 h-auto md:h-screen">
+        <div className=" relative flex flex-col w-full md:w-1/3 md:w-90">
           <div
-            className={`absolute right-0 w-[2px] h-16 bg-[#56ddc1] transition-transform duration-500 ease-in-out 
-    ${currentSelect === 1 ? "translate-y-16" : "translate-y-0"}`}
+            className={`absolute right-0 h-5 sm:h-16 w-[2px] bg-[#56ddc1] transition-transform duration-500 ease-in-out 
+    ${
+      currentSelect === 1 ? "translate-y-8 sm:translate-y-16 " : "translate-y-0"
+    }`}
           />
 
           <button
             onClick={() => {
               setCurrentSelect(2);
             }}
-            className={`text-left text-xl p-10 py-2  cursor-pointer hover:bg-gray-800 transition-all duration-300 ease-in-out ${
+            className={`text-left text-lg sm:text-xl p-10 py-2  cursor-pointer hover:bg-gray-800 transition-all duration-300 ease-in-out ${
               currentSelect == 2 ? "text-[#56ddc1]" : "text-[#8791af]"
             }`}
           >
@@ -62,24 +64,25 @@ export const Experience = () => {
             onClick={() => {
               setCurrentSelect(1);
             }}
-            className={`text-left text-xl p-10 py-2  cursor-pointer hover:bg-gray-800 transition-all  duration-300 ease-in-out ${
+            className={`text-left text-lg sm:text-xl p-10 py-2  cursor-pointer hover:bg-gray-800 transition-all  duration-300 ease-in-out ${
               currentSelect == 1 ? "text-[#56ddc1]" : "text-[#8791af]"
             }`}
           >
             DigiChum PVT LTD.
           </button>
         </div>
-        <div className="relative w-full overflow-hidden">
-          {data.map((item, key) => {
+        <div className="relative w-full overflow-visible md:overflow-hidden min-h-[300px] mt-4 md:mt-0">
+          {data.map((item) => {
+            const isActive = item.id === currentSelect;
+
+            const cls = isActive
+              ? "relative md:absolute md:top-0 md:left-0 z-10 opacity-100 translate-x-0"
+              : "absolute top-0 left-0 z-0 opacity-0 -translate-x-full pointer-events-none";
+
             return (
               <div
-                key={key}
-                className={`absolute top-0 left-0 w-full transition-all duration-700 ease-in-out transform 
-                    ${
-                      item.id === currentSelect
-                        ? "opacity-100 translate-x-0"
-                        : "opacity-0 -translate-x-full"
-                    }`}
+                key={item.id}
+                className={`w-full transition-all duration-700 ease-in-out transform ${cls}`}
               >
                 <CompanyDetails
                   companyPosition={item.companyPosition}
